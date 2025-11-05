bb88c4dea661b444746018f7680b87f36456815e
Reyn <jl88744653@gmail.com>
2025-11-06 01:41:37 +0800

    docs(dependency): 添加依赖项类的注释说明
    为Dependency类及其方法添加详细的JSDoc注释，提高代码可读性和维护性

--- assets/qin/dependency/dependency.ts
+++ assets/qin/dependency/dependency.ts
@@ -4,8 +4,15 @@ import { IDependency } from "../typings/dependency";
  * 依赖项基类
  */
 export class Dependency {
+  /** 依赖项注册回调 */
   onAttach(): void {}
+  /** 依赖项注销回调 */
   onDetach(): void {}
+  /**
+   * 获取依赖项
+   * @param name 依赖项名称
+   * @returns 
+   */
   dependencyOf<D extends IDependency>(name: string): D | undefined {
     return undefined;
   }
